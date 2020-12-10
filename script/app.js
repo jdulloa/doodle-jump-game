8b86d97653649fde678b693e65d103d0d5290310
JoseDanielMX <jduvmx@gmail.com>
2020-12-09 23:51:32 -0600

    Added a function for creating doodlers

--- script/app.js
+++ script/app.js
@@ -1,4 +1,14 @@
 document.addEventListener('DOMContentLoad', () => {
     const grid = document.querySelector('.grid')
     const doodler = document.createElement('div')
+    let doodlerLeftSpace = 50
+    let doodlerBottomSpace = 150
+
+    function createDoodler() {
+        grid.appendChild(doodler)
+        doodler.classList.add('doodler')
+        doodler.style.left = doodlerLeftSpace + 'px'
+        doodler.style.bottom = doodlerBottomSpace + 'px'
+    }
+    createDoodler()
 })
