53b771e821627deca2cc9916f827614a72c840d8
JoseDanielMX <jduvmx@gmail.com>
2020-12-10 01:15:51 -0600

    Added the logic for creating platforms

--- script/app.js
+++ script/app.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
     let doodlerLeftSpace = 50
     let doodlerBottomSpace = 150
     let isGameOver = false
+    let platformCount = 5
+    let platforms = []
 
     function createDoodler() {
         grid.appendChild(doodler)
@@ -12,9 +14,33 @@ document.addEventListener('DOMContentLoaded', () => {
         doodler.style.bottom = doodlerBottomSpace + 'px'
     }
 
+    class Platform {
+        constructor(newPlatformBottom) {
+            this.bottom = newPlatformBottom
+            this.left = Math.random() * 315    
+            this.visual = document.createElement('div')
+
+            const visual = this.visual
+            visual.classList.add('platform')
+            visual.style.left = this.left + 'px'
+            visual.style.bottom = this.bottom + 'px'
+            grid.appendChild(visual)
+        }
+    }
+
+    function createPlatforms() {
+        for (let i = 0; i < platformCount; i++) {
+            let platformGap = 600 / platformCount
+            let newPlatformBottom = 100 + i * platformGap
+            let newPlatform = new Platform(newPlatformBottom)
+            platforms.push(newPlatform)
+        }
+    }
+
     function start() {
         if (!isGameOver) {
             createDoodler()
+            createPlatforms()
         }
     }
     // Attach to button
