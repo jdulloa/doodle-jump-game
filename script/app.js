5b8c0e440b0d9ad8b405e7820a77872ab8e6a414
JoseDanielMX <jduvmx@gmail.com>
2020-12-10 21:16:26 -0600

    Added jump, fall and gameOver functions

--- script/app.js
+++ script/app.js
@@ -6,10 +6,13 @@ document.addEventListener('DOMContentLoaded', () => {
     let isGameOver = false
     let platformCount = 5
     let platforms = []
+    let upTimerId
+    let downTimerId
 
     function createDoodler() {
         grid.appendChild(doodler)
         doodler.classList.add('doodler')
+        doodlerLeftSpace = platforms[0].left
         doodler.style.left = doodlerLeftSpace + 'px'
         doodler.style.bottom = doodlerBottomSpace + 'px'
     }
@@ -47,11 +50,40 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    function jump() {
+        clearInterval(downTimerId)
+        upTimerId = setInterval(function() {
+            doodlerBottomSpace += 20
+            doodler.style.bottom = doodlerBottomSpace + 'px'
+            if (doodlerBottomSpace > 350) {
+                fall()
+            }
+        }, 30) 
+    }
+
+    function fall() {
+        clearInterval(upTimerId)
+        downTimerId = setInterval(function() {
+            doodlerBottomSpace -= 5
+            doodler.style.bottom = doodlerBottomSpace + 'px'
+            if (doodlerBottomSpace <= 0) {
+                gameOver()
+            }
+        }, 30)
+    }
+
+    function gameOver() {
+        isGameOver = true
+        clearInterval(upTimerId)
+        clearInterval(downTimerId)
+    }
+
     function start() {
         if (!isGameOver) {
-            createDoodler()
             createPlatforms()
+            createDoodler()
             setInterval(movePlatforms, 30)
+            jump()
         }
     }
     // Attach to button
