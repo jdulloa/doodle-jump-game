f1f6a956eec831f78877ef5571f1d89f81967140
JoseDanielMX <jduvmx@gmail.com>
2020-12-16 23:09:35 -0600

    Added the logic for showing the score

--- script/app.js
+++ script/app.js
@@ -14,6 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let isGoingRight = false
     let leftTimerId
     let rightTimerId
+    let score = 0
 
     function createDoodler() {
         grid.appendChild(doodler)
@@ -57,6 +58,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     let firstPlatform = platforms[0].visual
                     firstPlatform.classList.remove('platform')
                     platforms.shift()
+                    score++
                     let newPlatform = new Platform(600)
                     platforms.push(newPlatform)
                 }
@@ -105,6 +107,7 @@ document.addEventListener('DOMContentLoaded', () => {
         while (grid.firstChild) {
             grid.removeChild(grid.firstChild)
         }
+        grid.innerHTML = score
         clearInterval(upTimerId)
         clearInterval(downTimerId)
         clearInterval(leftTimerId)
